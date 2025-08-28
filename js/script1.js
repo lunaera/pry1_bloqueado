5ee73ec5aa4b3ccf3dc5018d560943e46ce1d4d3
Luna <erahinpachecoluna@gmail.com>
2025-08-27 22:40:24 -0600

    modificando commits

--- js/script1.js
+++ js/script1.js
@@ -2,4 +2,8 @@ console.log("hello");
 
 let a = 4;
 let b = 5;
+console.log(`el resultado de ${a} * ${b} es = ${a * b}`);
+
+console.log(`el resultado de ${a} * ${b} es = ${a * b}`);
+console.log(`el resultado de ${a} * ${b} es = ${a * b}`);
 console.log(`el resultado de ${a} * ${b} es = ${a * b}`); 
