1b79205465e335af5f6fb0c149f4544c846e6324
Luna <erahinpachecoluna@gmail.com>
2025-08-29 10:42:03 -0600

    nuevo comentario antes del revert

--- js/script1.js
+++ js/script1.js
@@ -6,4 +6,7 @@ console.log(`el resultado de ${a} * ${b} es = ${a * b}`);
 
 console.log(`el resultado de ${a} * ${b} es = ${a * b}`);
 console.log(`el resultado de ${a} * ${b} es = ${a * b}`);
-console.log(`el resultado de ${a} * ${b} es = ${a * b}`); 
+console.log(`el resultado de ${a} * ${b} es = ${a * b}`);
+
+
+// yo soy un comentario antes de aplicar revert
