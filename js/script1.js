7866db818381d0be9f06c8e46e38f7576242fca0
Luna <erahinpachecoluna@gmail.com>
2025-08-29 10:42:44 -0600

    Revert "nuevo comentario antes del revert"
    This reverts commit 1b79205465e335af5f6fb0c149f4544c846e6324.

--- js/script1.js
+++ js/script1.js
@@ -6,7 +6,4 @@ console.log(`el resultado de ${a} * ${b} es = ${a * b}`);
 
 console.log(`el resultado de ${a} * ${b} es = ${a * b}`);
 console.log(`el resultado de ${a} * ${b} es = ${a * b}`);
-console.log(`el resultado de ${a} * ${b} es = ${a * b}`);
-
-
-// yo soy un comentario antes de aplicar revert
+console.log(`el resultado de ${a} * ${b} es = ${a * b}`); 
