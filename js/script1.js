9a0da90e202e429abc1a4df010c649598a3dbd00
Luna <erahinpachecoluna@gmail.com>
2025-08-29 10:44:57 -0600

    array pets

--- js/script1.js
+++ js/script1.js
@@ -4,6 +4,13 @@ let a = 4;
 let b = 5;
 console.log(`el resultado de ${a} * ${b} es = ${a * b}`);
 
+
+let arrayNamepets = ["puppy", "kitty", "dog", "cat"];
+
+arrayNamepets.forEach(element => {
+    console.log(element);
+});
+
 console.log(`el resultado de ${a} * ${b} es = ${a * b}`);
 console.log(`el resultado de ${a} * ${b} es = ${a * b}`);
 console.log(`el resultado de ${a} * ${b} es = ${a * b}`); 
